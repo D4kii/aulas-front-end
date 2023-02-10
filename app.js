96290fcf0f7dda36fffbad7a7d6d2b25d3359bb8
Dan Lino <danielalinodasilvaoliveira@gmail.com>
2023-02-09 22:19:00 -0300

    update 09/02/23

--- app.js
+++ app.js
@@ -4,18 +4,31 @@
 const calcular = document.getElementById('calcular');
 
 function somar() {
+    const formulario = document.getElementById('formulario');
     const nota1 = Number(document.getElementById('nota1').value);
     const nota2 = Number(document.getElementById('nota2').value);
+    let nota_final = document.getElementById('nota_final');
     let situacao = document.getElementById('situacao');
-    const resultado;
-    resultado.value = (nota1 +nota2) /2;
+    const resultado = Number((nota1 + nota2) /2);
 
-    if(resultado > 5){
-        situacao.classList.add('sombra-vermelha')
-        
+
+
+    if(resultado < 5){
+        situacao.value = `Reprovado`;
+        nota_final.value = resultado;
+        formulario.classList.add('sombra-vermelha');
+        situacao.classList.add('sombra-vermelha');
+
     }else{
-        return(situacao.classList.add('sombra-verde'))
-        situacao = 'aprovado';
+        situacao.value = `Aprovado!`;
+        nota_final.value = resultado;
+        formulario.classList.add('sombra-azul');
+        situacao.classList.add('sombra-azul');
+
     }
 
 }
+
+calcular.addEventListener('click', somar);
+
+
